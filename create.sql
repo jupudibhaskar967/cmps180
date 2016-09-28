bf49a98f3a21cc41c890c3574aaf2ac709bd0dd5
Naga Venkata Sai Indubhaskar Jupudi <njupudi@be-linux27.lt.ucsc.edu>
2016-09-28 16:53:02 -0700

    lab1

---
+++ create.sql
@@ -0,0 +1,39 @@
+DROP SCHEMA lab1 CASCADE;
+CREATE SCHEMA lab1;
+
+CREATE TABLE STORES(
+   store_id INT PRIMARY KEY,
+   address	TEXT,
+   manager	TEXT
+);
+
+
+
+CREATE TABLE Customers(
+   customer_id INT PRIMARY KEY,
+   name		TEXT,
+   address	TEXT,
+   email        TEXT
+);
+
+
+
+
+CREATE TABLE Products(
+   product_id INT PRIMARY KEY,
+   name		TEXT,
+   category	TEXT,
+   manufacturer	TEXT
+);
+
+
+
+CREATE TABLE Sales(
+   store_id		INT PRIMARY KEY,	
+   customer_id		INT,
+   product_id		INT,
+   date_p		TEXT,
+   quantity		INT,
+   price		INT,
+   shipped		BOOLEAN
+);
