902c340dcd4263f86a2a86e1d2d9bd809fd487cc
Bhaskar <njupudi@ucsc.edu>
2016-11-12 12:54:49 -0800

    changed merge.sql

--- lab3/merge.sql
+++ lab3/merge.sql
@@ -1,16 +1,2 @@
-DROP VIEW view1 cascade;
-CREATE VIEW view1 AS 
-select store_id,customer_id,product_id,purchase_date,quantity,unit_price from sales;
-
-
-CREATE VIEW view2 AS 
-select * from newsales
-EXCEPT
-select * from view1;
-
-
-
 insert into sales
-select store_id,customer_id,product_id,purchase_date,quantity,unit_price,'false' from view2 where (store_id,customer_id,product_id,purchase_date,quantity) not in (select store_id,customer_id,product_id,purchase_date,quantity from sales);
-
-
+select store_id,customer_id,product_id,purchase_date,quantity,unit_price,'false' from newsales where (newsales.store_id,newsales.customer_id,newsales.product_id,newsales.purchase_date) not in (select sales.store_id,sales.customer_id,sales.product_id,sales.purchase_date from sales);
